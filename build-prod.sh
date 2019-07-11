693158a8259f060a67fd873883f758731e470660
ElkanRoelen <roelen@elkan.nl>
2019-07-11 15:20:33 +0200

    updated builds :

--- build-prod.sh
+++ build-prod.sh
@@ -5,3 +5,12 @@ $(aws ecr get-login --no-include-email --region eu-central-1)
 docker build --no-cache -t didux/bis:prod .
 docker tag didux/bis:prod 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/bis:prod
 docker push 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/bis:prod
+
+docker build --no-cache -t  didux/bis:arm -f Dockerfile-arm .
+docker tag didux/bis:arm 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/bis:arm
+docker push 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/bis:arm
+
+docker tag didux/bis:arm diduxio/bis:arm
+docker tag didux/bis:prod diduxio/bis:latest
+docker push diduxio/bis:arm
+docker push diduxio/bis:latest
--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-$(aws ecr get-login --no-include-email --region eu-central-1)
-
-docker build --no-cache -t didux/bis:test .
-docker tag didux/bis:test 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/bis:test
-docker push 462619610638.dkr.ecr.eu-central-1.amazonaws.com/didux/bis:test
+docker build --no-cache -t diduxio/bis:test .
